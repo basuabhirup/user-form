0007d289e072f59a5c82e8cc56789f021c81d936
basuabhirup <basu.abhirup@yahoo.com>
2020-08-26 23:33:06 +0530

    Updates

---
+++ precache-manifest.4ae71968992fa2a539614bdfb1d9a349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766b4a83f426dea1c6de4f1f2bb57e1b",
+    "url": "/user-form/index.html"
+  },
+  {
+    "revision": "c803fa7c017f3e8730c4",
+    "url": "/user-form/static/css/main.5695f1ee.chunk.css"
+  },
+  {
+    "revision": "d5eeb704815747bb606a",
+    "url": "/user-form/static/js/2.6f7596fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-form/static/js/2.6f7596fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c803fa7c017f3e8730c4",
+    "url": "/user-form/static/js/main.4171611a.chunk.js"
+  },
+  {
+    "revision": "9795f57e67c56edecb56",
+    "url": "/user-form/static/js/runtime-main.a914fd1c.js"
+  }
+]);
